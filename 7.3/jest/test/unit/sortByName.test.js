ecd961969db183fc903655788d34dd60841d9789
kseniia <kshamtova22@mail.ru>
2021-11-12 10:47:28 +0300

    coverage

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,24 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
-  });
+	it("Books names should be sorted in ascending order", () => {
+		expect(
+			sorting.sortByName([
+				"Гарри Поттер",
+				"Властелин Колец",
+				"Волшебник изумрудного города",
+			])
+		).toEqual([
+			"Властелин Колец",
+			"Волшебник изумрудного города",
+			"Гарри Поттер",
+		]);
+	});
+
+	it("Books names should be sorted in ascending order 2 (no moves)", () => {
+		expect(sorting.sortByName(["Властелин Колец", "Властелин Колец"])).toEqual([
+			"Властелин Колец",
+			"Властелин Колец",
+		]);
+	});
 });
