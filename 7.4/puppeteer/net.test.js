e1d5ab54fa2f7de9b88d2f28942ee2e46421a5b6
kseniia-QA <93254804+kseniia-QA@users.noreply.github.com>
2021-11-13 10:52:00 +0300

    Update net.test.js

--- 7.4/puppeteer/net.test.js
+++ 7.4/puppeteer/net.test.js
@@ -1,5 +1,7 @@
 let page;
 
+describe("Netology.ru tests", () => {
+
 beforeEach(async () => {
   page = await browser.newPage();
   await page.goto("https://netology.ru");
@@ -9,22 +11,21 @@ afterEach(() => {
   page.close();
 });
 
-describe("Netology.ru tests", () => {
   test("The first test'", async () => {
     const title = await page.title();
     console.log("Page title: " + title);
     const firstLink = await page.$("header a + a");
-    // const firstLinkText = await page.$eval(
-    //   "header a + a",
-    //   (link) => link.textContent
-    // );
+    const firstLinkText = await page.$eval(
+      "header a + a",
+      (link) => link.textContent
+    );
     await firstLink.click();
-    await page.waitForNavigation();
+    await page.waitForNavigation(5000);
     const title2 = await page.title();
     console.log("Page title: " + title2);
     const pageList = await browser.newPage();
     await pageList.goto("https://netology.ru/navigation");
-    await pageList.waitForSelector("h1");
+    await pageList.waitForSelector("h1"); 
   });
 
   test("The first link text 'Медиа Нетологии'", async () => {
@@ -39,5 +40,32 @@ describe("Netology.ru tests", () => {
     });
     const actual = await page.$eval(".logo__media", (link) => link.textContent);
     expect(actual).toContain("Медиа");
+
   });
-});
+
+ describe("Homework Suite", () => {
+
+ test("Checks page with vacancies'", async () => { 
+  page = await browser.newPage();
+      await page.goto("https://netology.ru/job");
+      const actual = await page.$eval("h1.shared-containers-Jobs-components-Presentation-presentation-module__title--gkLaB", (link) => link.textContent);
+    expect(actual).toContain("Работа ");
+    })
+
+test("Checks page with experts", async () => { 
+  page = await browser.newPage();
+      await page.goto("https://netology.ru/experts");
+      const actual = await page.$eval("h1.tn-atom", (link) => link.textContent);
+    expect(actual).toContain("Станьте экспертом"); 
+
+
+    })
+test("Checks page with partners", async () => { 
+  page = await browser.newPage();
+      await page.goto("https://netology.ru/partners");
+      const actual = await page.$eval("h1.shared-containers-Partners-components-Header-header-module__title--1vdjP", (link) => link.textContent);
+    expect(actual).toContain("Станьте партнером Нетологии"); 
+
+})
+})
+}) 
