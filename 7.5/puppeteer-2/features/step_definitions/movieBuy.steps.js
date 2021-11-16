30b87fba3648c6abd9c2728077928ed3e9df7d13
kseniia-QA <93254804+kseniia-QA@users.noreply.github.com>
2021-11-16 11:46:23 +0300

    Update and rename search.steps.js to movieBuy.steps.js

---
+++ 7.5/puppeteer-2/features/step_definitions/movieBuy.steps.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+const chai = require('chai');
+const expect = chai.expect;
+const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
+const { clickElement, putText, getText } = require('../../lib/commands.js');
+
+Before(async function () {
+	const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+	const page = await browser.newPage();
+	this.browser = browser;
+	this.page = page;
+});
+
+After(async function () {
+	if (this.browser) {
+		await this.browser.close();
+	}
+});
+
+
+
+Given('user is on {string} page', async function (string) {
+	return await this.page.goto(`${string}`, {
+		setTimeout: 20000,
+	});
+});
+
+When('user choose day {string}', async function (string) {
+	await clickElement(this.page, `${string}`); //choose day
+});
+When('user choose time {string}', async function (string) {
+	await clickElement(this.page, `${string}`); //choose time
+});
+When('user choose chair {string}', async function (string) {
+	await clickElement(this.page, `${string}`); //choose chair
+});
+When('user click booking {string}', async function (string) {
+	await clickElement(this.page, `${string}`); //click booking
+
+Then('user sees text {string}', async function (string) {
+    const actual = await getText(this.page, 'h2.ticket__check-title');
+    const expected = await string;
+    expect(actual).contains(expected);
+  });
+});
+
+
